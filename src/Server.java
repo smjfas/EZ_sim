21040819019004872dd2857c6d5058fe0c88fd4b
Ali Gorji <alli.gorji@gmail.com>
2018-06-25 16:28:47 +0430

    Start java coding

---
+++ src/Server.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+/**
+ * Created by ali on 6/25/18.
+ */
+public class Server {
+    ArrayList<Work> workList = new ArrayList<>();
+    
+}
---
+++ src/Work.java
@@ -0,0 +1,18 @@
+/**
+ * Created by ali on 6/25/18.
+ */
+public class Work {
+    int time;
+
+    public Work(int time) {
+        this.time = time;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+}
