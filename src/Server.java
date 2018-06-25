3615f60ee966335ebdc55dc21be59f88dd9695fa
smjfas <smjfas@gmail.com>
2018-06-25 16:56:09 +0430

    server capacity and addWork

--- src/Server.java
+++ src/Server.java
@@ -4,18 +4,28 @@ import java.util.ArrayList;
  * Created by ali on 6/25/18.
  */
 public abstract class Server {
-    private ArrayList<Work> workList;
-    int mu;
+    private ArrayList<Work> workList = new ArrayList<>();
+    private int mu;
+    private int capacity;
 
-    public Server(int mu) {
-        workList = new ArrayList<>();
+    public Server(int mu, int capacity) {
         this.mu = mu;
+        this.capacity = capacity;
     }
 
     abstract void doWork();
 
     public int getMu() {
         return mu;
+    }
+    
+    public boolean addWork(Work work){
+        if (workList.size() >= capacity){
+            return false;
+        } else {
+            workList.add(work);
+            return true;
+        }
     }
 
     public void setMu(int mu) {
